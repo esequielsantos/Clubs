eff3d70d3ba15a21fe3c3fc5f9099972c7e5bb29
Edison Santos <edisones@ciasc.sc.gov.br>
2025-02-17 15:06:06 -0300

    Correct db connection viwh ormconfig

--- api/ormconfig.ts
+++ api/ormconfig.ts
@@ -2,9 +2,8 @@ import { DataSourceOptions } from 'typeorm';
 
 export const ormconfig: DataSourceOptions = {
   type: 'sqlite',
-  name: 'dataConnection',
   database: 'club.db',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  migrations: ['src/migrations/*{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*.{.ts,.js}'],
   synchronize: true,
 };
