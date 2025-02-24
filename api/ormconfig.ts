a15c67eb15e59b4bde96c90a0c6404e2205df2a6
Edison Santos <edisones@ciasc.sc.gov.br>
2025-02-24 11:54:42 -0300

    Create database external from applicattion

--- api/ormconfig.ts
+++ api/ormconfig.ts
@@ -2,7 +2,7 @@ import { DataSourceOptions } from 'typeorm';
 
 export const ormconfig: DataSourceOptions = {
   type: 'sqlite',
-  database: 'club.db',
+  database: '../../ClubsDb/club.db',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*.{.ts,.js}'],
   synchronize: true,
